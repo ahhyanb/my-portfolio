24f2484b0949b55e32dcdde59afb68cc1e859421
Ryan Bartolome <ahhyann@icloud.com>
2025-01-10 00:02:49 -1000

    added Projects component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/home/Home';
 import Bio from './components/bio/Bio';
+import Projects from './components/projects/Project';
 
 function App() {
   return (
    <>
     <Routes>
+
       <Route path="/" element={<Header />} />
       <Route path="/home" element={<Home />} />
       <Route path="/bio" element={<Bio />} />
+      <Route path="/projects" element={<Projects />} />
+
     </Routes>
   </>
   );
