1eb2fe52f3507d74167fd17f06394cec861b1bca
Ryan Bartolome <rahhyahh@icloud.com>
2025-01-09 16:56:19 -1000

    created the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import "./Header.css"; 
+
 function Header() {
-    return ( 
-        <div>
-            <h1>ryan bartolome</h1>
-        </div>
-       
-     );
+    return (
+        <>
+            <header className="header">
+                <div className="logo-container">
+                    <span className="logo">rb</span>
+                    <span className="divider"></span>
+                    <h1 className="name">ryan bartolome</h1>
+                </div>
+                <nav className="nav-container">
+                    <Link to="/" className="nav-link">home</Link>
+                    <span className="divider"></span>
+                    <Link to="/about" className="nav-link">about</Link>
+                    <span className="divider"></span>
+                    <Link to="/projects" className="nav-link">projects</Link>
+                    <span className="divider"></span>
+                    <Link to="/contact" className="nav-link contact-link">contact me</Link>
+                </nav>
+            </header>
+        </>
+    );
 }
 
 export default Header;
