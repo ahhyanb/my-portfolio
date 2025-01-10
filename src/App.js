7ba5eae027b84ed94bf5a6896a8b5449068df61e
Ryan Bartolome <ahhyann@icloud.com>
2025-01-09 23:47:31 -1000

    added Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
    <>
     <Routes>
       <Route path="/" element={<Header />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,12 @@ function Header() {
                 <div className="logo-container">
                     <span className="logo">rb</span>
                     <span className="divider"></span>
-                    <h1 className="name">ryan bartolome</h1>
+                    <h1 className="name">ryan mart bartolome</h1>
                 </div>
                 <nav className="nav-container">
                     <Link to="/" className="nav-link">home</Link>
                     <span className="link-divider"></span>
-                    <Link to="/about" className="nav-link">about</Link>
+                    <Link to="/about" className="nav-link">bio</Link>
                     <span className="link-divider"></span>
                     <Link to="/projects" className="nav-link">projects</Link>
                     <span className="link-divider"></span>
