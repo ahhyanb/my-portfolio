54cbac2a7cb6f2b4a61cbdff0a91e1f83b84b107
Ryan Bartolome <ahhyann@icloud.com>
2025-01-10 00:05:19 -1000

    added the Contact component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Router } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/home/Home';
 import Bio from './components/bio/Bio';
 import Projects from './components/projects/Project';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/bio" element={<Bio />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
 
     </Routes>
   </>
