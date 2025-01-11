4f7529431236192aad8d2f0602a75ed6b71d4593
Ryan Bartolome <ahhyann@icloud.com>
2025-01-10 14:51:41 -1000

    added picture and styling in the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,24 @@
 import "./Home.css";
 
-function Home () {
+function Home() {
     return (
         <div className="container">
-            <h1>hello, 
-                <br />
-                <span className="highlight">i'm ryan</span>
-            </h1>
-            <h2>
-                full-stack software engineer
-            </h2>
-        </ div>
-    )
+            <div className="text-container">
+                <h1>
+                    hello!
+                     <br />
+                    <span className="highlight">i'm ryan,</span>
+                </h1>
+                <h2>full-stack software engineer</h2>
+            </div>
+            
+            <img
+                src="./portrait-transparent.png"
+                alt="Ryan's Portrait"
+                className="portrait"
+            />
+        </div>
+    );
 }
 
 export default Home;
